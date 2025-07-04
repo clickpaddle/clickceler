e9255adbed41d44c82d5d3d9a2f2451614b795cc
luc <luc@trex.localdomain>
2025-07-04 22:37:15 +0200

    Update lang

--- src/C/core/jollyclick.c
+++ src/C/core/jollyclick.c
@@ -20,14 +20,14 @@ typedef struct {
     intptr_t thread_logical_id; // ID logique (0 pour event, 1 pour query, 2 pour severity)
 } thread_arg_t;
 
-// Fonction utilitaire pour charger les règles Prolog pour un thread donné
+//Load rules for Prolog Thread 
 static int load_rules_for_thread(intptr_t tid, module_t *out_module) {
     char modname[32];
     snprintf(modname, sizeof modname, "thread_%ld", (long)tid);
     *out_module = PL_new_module(PL_new_atom(modname));
 
     char filename[64];
-    // Les fichiers de règles sont maintenant nommés directement par leur ID logique
+    // Naming of rulesets.
     snprintf(filename, sizeof filename, "../rules/rules_%ld.pl", (long)tid);
 
     char goal_buf[256];
@@ -41,7 +41,7 @@ static int load_rules_for_thread(intptr_t tid, module_t *out_module) {
         return 0;
     }
 
-    // Appeler thread_goal(tid) si défini (bonne pratique pour l'initialisation du module)
+    // Init Module 
     predicate_t p = PL_predicate("thread_goal", 1, modname);
     term_t t = PL_new_term_ref(); PL_put_integer(t, (long)tid);
     (void)PL_call_predicate(*out_module, PL_Q_NORMAL, p, t);
@@ -49,7 +49,7 @@ static int load_rules_for_thread(intptr_t tid, module_t *out_module) {
     return 1;
 }
 
-// Thread pour la collecte des événements (client_id 0)
+// Thread 0 to collect events
 void *event_collector_handler(void *arg) {
     thread_arg_t *targ = (thread_arg_t *)arg;
     int sockfd = targ->sockfd;
@@ -72,17 +72,17 @@ void *event_collector_handler(void *arg) {
     char buffer[MAX_LINE];
     ssize_t nbytes;
 
-    // Assurez-vous que assert_json_event/2 est défini dans rules_0.pl
+    // assert_json_event/2 s in rules_0.pl ?
     predicate_t p_assert = PL_predicate("assert_json_event", 2, mod);
     if (!p_assert) {
-        fprintf(stderr, "[ERR] Event Collector Thread %ld: Prédicat 'assert_json_event/2' introuvable dans le module '%s'.\n", logical_id, PL_module_name(mod));
+        fprintf(stderr, "[ERR] Event Collector Thread %ld: Can't find predicate 'assert_json_event/2' in ruleset '%s'.\n", logical_id, PL_module_name(mod));
         PL_thread_destroy_engine();
         close(sockfd);
         return NULL;
     }
 
 
-    printf("[Thread Collecte %ld] Prêt à recevoir des événements JSON.\n", logical_id);
+    printf("[Thread Collect %ld] ready to received JSON events JSON.\n", logical_id);
     while ((nbytes = recv(sockfd, buffer, sizeof(buffer) - 1, 0)) > 0) {
         buffer[nbytes] = '\0';
         printf("[Thread Collecte %ld] Reçu JSON: %s\n", logical_id, buffer);
@@ -107,7 +107,7 @@ void *event_collector_handler(void *arg) {
     }
 
     if (nbytes == 0)
-        printf("[Thread Collecte %ld] Client déconnecté\n", logical_id);
+        printf("[Thread Collect %ld] Client is disconnected\n", logical_id);
     else if (nbytes < 0)
         perror("recv [Thread Collecte]");
 
@@ -116,7 +116,7 @@ void *event_collector_handler(void *arg) {
     return NULL;
 }
 
-// Thread pour l'exécution des requêtes par un client externe (client_id 1)
+// Thread to execute the clien_1 ruleset eules_1.pl
 void *query_executor_handler(void *arg) {
     thread_arg_t *targ = (thread_arg_t *)arg;
     int sockfd = targ->sockfd;
@@ -130,7 +130,7 @@ void *query_executor_handler(void *arg) {
     }
 
     module_t mod;
-    if (!load_rules_for_thread(logical_id, &mod)) { // Charge rules_1.pl
+    if (!load_rules_for_thread(logical_id, &mod)) { // load rules_1.pl
         PL_thread_destroy_engine();
         close(sockfd);
         return NULL;
@@ -139,39 +139,39 @@ void *query_executor_handler(void *arg) {
     char buffer[MAX_LINE];
     ssize_t nbytes;
 
-    printf("[Thread Requêtes %ld] Prêt à recevoir des requêtes Prolog.\n", logical_id);
+    printf("[Thread  Request  %ld] edy to receive queries Prolog.\n", logical_id);
     while ((nbytes = recv(sockfd, buffer, sizeof(buffer) - 1, 0)) > 0) {
         buffer[nbytes] = '\0';
-        printf("[Thread Requêtes %ld] Reçu requête: %s\n", logical_id, buffer);
+        printf("[Thread Request %ld] Query received: %s\n", logical_id, buffer);
 
         term_t goal = PL_new_term_ref();
-        // Convertir la chaîne reçue en terme Prolog (but)
+        // Converrt string in Prolog terms (goal)
         if (!PL_chars_to_term(buffer, goal)) {
-            fprintf(stderr, "[Thread Requêtes %ld] Erreur: impossible de parser la requête '%s'.\n", logical_id, buffer);
+            fprintf(stderr, "[Thread Request %ld] Error: ersing eror of the request '%s'.\n", logical_id, buffer);
             send(sockfd, "Error: Invalid query format.\n", 28, 0);
             continue;
         }
 
-        // Exécuter la requête
+        // Execute query 
         qid_t q = PL_open_query(mod, PL_Q_CATCH_EXCEPTION, goal, NULL);
         if (PL_next_solution(q)) {
-            // Si la requête réussit, envoyer une confirmation ou le résultat
+            // at
             send(sockfd, "Query successful.\n", 18, 0);
         } else {
             term_t ex = PL_exception(q);
             if (ex) {
                 char *ex_str = NULL;
                 if (PL_get_chars(ex, &ex_str, CVT_ALL)) {
-                    fprintf(stderr,"[Thread Requêtes %ld] Prolog Error: %s\n", logical_id, ex_str);
+                    fprintf(stderr,"[Thread Request %ld] Prolog Error: %s\n", logical_id, ex_str);
                     char response_buf[MAX_LINE];
                     snprintf(response_buf, sizeof(response_buf), "Prolog Error: %s\n", ex_str);
                     send(sockfd, response_buf, strlen(response_buf), 0);
                 } else {
-                    fprintf(stderr,"[Thread Requêtes %ld] Prolog Error (non-stringifiable exception)\n", logical_id);
+                    fprintf(stderr,"[Thread Request %ld] Prolog Error (non-stringifiable exception)\n", logical_id);
                     send(sockfd, "Prolog Error (non-stringifiable exception)\n", 44, 0);
                 }
             } else {
-                fprintf(stderr,"[Thread Requêtes %ld] Query failed (no exception caught).\n", logical_id);
+                fprintf(stderr,"[Thread Request %ld] Query failed (no exception caught).\n", logical_id);
                 send(sockfd, "Query failed.\n", 14, 0);
             }
         }
@@ -179,18 +179,18 @@ void *query_executor_handler(void *arg) {
     }
 
     if (nbytes == 0)
-        printf("[Thread Requêtes %ld] Client déconnecté\n", logical_id);
+        printf("[Thread Request %ld] Client is disconnected\n", logical_id);
     else if (nbytes < 0)
-        perror("recv [Thread Requêtes]");
+        perror("recv [Thread Request]");
 
     close(sockfd);
     PL_thread_destroy_engine();
     return NULL;
 }
 
-// Thread pour la mise à jour de la sévérité (client_id 2)
+// Thread  to run internal rules (client_id 2)
 void *severity_updater_thread(void *arg) {
-    intptr_t logical_id = (intptr_t)arg; // ID logique 2 pour ce thread
+    intptr_t logical_id = (intptr_t)arg; // ID 2 for this thread 
 
     if (!PL_thread_attach_engine(NULL)) {
         fprintf(stderr, "[ERR] Severity Updater Thread %ld: PL_thread_attach_engine failed\n", logical_id);
@@ -198,24 +198,24 @@ void *severity_updater_thread(void *arg) {
     }
 
     module_t mod;
-    if (!load_rules_for_thread(logical_id, &mod)) { // Charge rules_2.pl
+    if (!load_rules_for_thread(logical_id, &mod)) { // Load rules_2.pl
         PL_thread_destroy_engine();
         return NULL;
     }
 
-    printf("[Thread Sévérité %ld] Démarré. Appel de update_event_severity...\n", logical_id);
+    printf("[Thread internal rules %ld] started. Call update_event_severity...\n", logical_id);
 
-    // Assurez-vous que update_event_severity/0 est défini dans rules_2.pl
+    // Check that update_event_severity/0 is defined in dans rules_2.pl
     predicate_t p_update_severity = PL_predicate("update_event_severity", 0, mod);
     if (!p_update_severity) {
-        fprintf(stderr, "[ERR] Thread Sévérité %ld: Prédicat 'update_event_severity/0' introuvable dans le module '%s'.\n", logical_id, PL_module_name(mod));
+        fprintf(stderr, "[ERR] Thread Internal rules %ld: Predicate 'update_event_severity/0' s not in ruleset '%s'.\n", logical_id, PL_module_name(mod));
         PL_thread_destroy_engine();
         return NULL;
     }
 
-    // Boucle pour l'exécution périodique de la règle de sévérité
+    //loop to  Execute internal rules
     while (1) {
-        printf("[Thread Sévérité %ld] Exécution périodique de update_event_severity...\n", logical_id);
+        printf("[Thread internal rule %ld]  loop execution update_event_severity...\n", logical_id);
         qid_t q_periodic = PL_open_query(mod, PL_Q_CATCH_EXCEPTION, p_update_severity, 0);
         if (!PL_next_solution(q_periodic)) {
             term_t ex = PL_exception(q_periodic);
@@ -230,19 +230,19 @@ void *severity_updater_thread(void *arg) {
                 fprintf(stderr,"[Thread Sévérité %ld] Periodic update_event_severity failed (no exception caught).\n", logical_id);
             }
         } else {
-            printf("[Thread Sévérité %ld] Periodic update_event_severity a été exécuté avec succès.\n", logical_id);
+            printf("[Thread internal rule %ld] Periodic update_event_severity was successful.\n", logical_id);
         }
         PL_close_query(q_periodic);
-        sleep(10); // Mettre à jour toutes les 10 secondes (ajustez si besoin)
+        sleep(10); // update every 10 second (ajustez si besoin)
     }
 
-    // Ce code ne sera jamais atteint si la boucle while(1) est active
+    // Never executed 
     PL_thread_destroy_engine();
     return NULL;
 }
 
 
-// Fonction utilitaire pour configurer et lancer un serveur d'écoute
+// Launch server and listen 
 int setup_and_listen_server(int port) {
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (server_fd < 0) { perror("[ERR] socket"); return -1; }
@@ -262,7 +262,7 @@ int setup_and_listen_server(int port) {
     if (listen(server_fd, MAX_CLIENTS) < 0) {
         perror("[ERR] listen"); close(server_fd); return -1;
     }
-    printf("[INFO] Serveur d'écoute démarré sur le port %d\n", port);
+    printf("[INFO] Server started and listening on port %d\n", port);
     return server_fd;
 }
 
@@ -270,11 +270,11 @@ int setup_and_listen_server(int port) {
 int main(int argc, char **argv) {
     char *plav[2] = {argv[0], "-q"};
     if (!PL_initialise(2, plav)) {
-        fprintf(stderr, "[ERR] Echec initialisation SWI-Prolog\n");
+        fprintf(stderr, "[ERR] Failure to initialize SWI-Prolog\n");
         return 1;
     }
 
-    // Lancer le thread de mise à jour de la sévérité (ID logique 2)
+    // Launch thread internal rules (ID 2)
     pthread_t severity_pthread_id;
     if (pthread_create(&severity_pthread_id, NULL, severity_updater_thread, (void*)2) != 0) {
         perror("[ERR] pthread_create severity_updater_thread");
@@ -283,16 +283,16 @@ int main(int argc, char **argv) {
     pthread_detach(severity_pthread_id);
 
 
-    // Démarrer le serveur d'écoute pour la collecte des événements (ID logique 0)
+    // Launch server and listen for events (ID logique 0)
     int event_server_fd = setup_and_listen_server(SERVER_PORT_EVENTS);
     if (event_server_fd < 0) { PL_halt(1); }
 
-    // Démarrer le serveur d'écoute pour les requêtes externes (ID logique 1)
+    // Luanch server and listen for external queries (ID  1)
     int query_server_fd = setup_and_listen_server(SERVER_PORT_QUERIES);
     if (query_server_fd < 0) { close(event_server_fd); PL_halt(1); }
 
 
-    // Boucle principale pour accepter les connexions
+    // Loop to accept TCP connections 
     while (1) {
         fd_set read_fds;
         FD_ZERO(&read_fds);
@@ -341,7 +341,7 @@ int main(int argc, char **argv) {
             if (!targ) { fprintf(stderr, "[ERR] malloc\n"); close(new_sock); continue; }
 
             targ->sockfd = new_sock;
-            targ->thread_logical_id = 1; // ID logique 1 pour le client de requêtes
+            targ->thread_logical_id = 1; // ID 1 for the client_1 
 
             pthread_t tid;
             if (pthread_create(&tid, NULL, query_executor_handler, targ) != 0) {
@@ -351,7 +351,7 @@ int main(int argc, char **argv) {
                 continue;
             }
             pthread_detach(tid);
-            printf("[INFO] Nouvelle connexion sur le port requêtes (Exécuteur #1)\n");
+            printf("[INFO] New connection for queries (Exécuteur #1)\n");
         }
     }
 
