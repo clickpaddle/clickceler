bc2fbd87f203ba2669bc619a8b82ea7545a5f1fa
Luc Dijon <144903516+clickpaddle@users.noreply.github.com>
2025-08-01 03:08:16 +0200

    Delete src/C/refine.pl
    Signed-off-by: Luc Dijon <144903516+clickpaddle@users.noreply.github.com>

--- src/C/refine.pl
+++
@@ -1,6 +0,0 @@
-refine_rule( server, 100, 'close_low_severity', [ eq(status, open), eq(severity, critical) ], [ set_field(status, closed),set_field(severity, warning) ]).
-refine_rule( log, 100, 'close_low_severity', [ eq(status, open), eq(severity, critical) ], [ set_field(status, closed),set_field(severity, warning) ]).
-refine_rule( disk, 100, 'close_low_severity', [ eq(status, open), eq(severity, critical) ], [ set_field(status, closed),set_field(severity, warning) ]).
-refine_rule( memory, 100, 'close_low_severity', [ eq(status, open), eq(severity, critical) ], [ set_field(status, closed),set_field(severity, warning) ]).
-refine_rule( cpu, 100, 'close_low_severity', [ eq(status, open), eq(severity, critical) ], [ set_field(status, closed),set_field(severity, warning) ]).
-
