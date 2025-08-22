147ba5003774ca626bd5ccf7c277e5f80955adb7
Luc Dijon <luc.dijon@clickpaddle.com>
2025-08-22 18:19:57 +0200

    Complted abstract rules design in ../rules and ../yaml

--- src/C/rules/filter.pl
+++ src/C/rules/filter.pl
@@ -1,19 +1,11 @@
-filter_rule(
-    filter_no_pass_log_critical_dev_drop_event,
-    100,
-    [memory(E)],
-    [within(E, severity, [critical, info, ok]), eq(E, env, dev)],
-    [nopass]
-).
 
-filter_rule(
-    filter_pass_log_major_prod_event,
-    100,
-    [memory(E)],
-    [
-     within(E, severity, [major,fatal]), 
-     eq(E, env, prod)
-    ],
-    [pass]
+filter_rule(filter_no_pass_log_critical_dev_drop_event, 100,
+    [ cpu(E) ],
+    [within(E, severity, [info, ok]), eq(E, env, dev) ],
+    [nopass ]
 ).
-
+filter_rule(filter_pass_log_major_prod_event, 100,
+    [ server(E) ],
+    [within(E, severity, [warning, major, critical, fatal]), eq(E, env, prod) ],
+    [pass ]
+).
--- src/C/rules/throttle.pl
+++ src/C/rules/throttle.pl
@@ -1,10 +1,7 @@
-% Auto-generated throttle rules from YAML
 
-throttle_rule(
-    throttle_network_event,
-    100,
-    ['snmptrap(E)'],
-    ['within(E, severity, [info])', 'eq(E, env, dev)', 'contains(E, msg, "Hello World!")'],
-    [settings{'limit': '2'}, settings{'window': '30s'}, settings{'delay': '15s'}],
-    ['send_first']
+throttle_rule(throttle_network_event, 100,
+    [ snmptrap(E) ],
+    [within(E, severity, [info]), eq(E, env, dev), contains(E, msg, "Hello World!") ],
+    [settings{"limit": 2}, settings{"window": "30s"}, settings{"delay": "15s"} ],
+    [send_first ]
 ).
